41398b2f2624ffd1dd99f8f20d44f49ef2fcbed8
Malcolm Preston <malcolm@thunderboltlabs.com>
2015-06-24 17:05:51 -0500

    adding configuration script

---
+++ monolith_config.ps1
@@ -0,0 +1,17 @@
+﻿
+$destination = "C:\hudl\config"
+$source = "\\fs01.agilesports.local\DevShare\first-time-config"
+
+Write-Output "Downloading configuration files from $source`n"
+
+Copy-Item -Path "$source\*" -Destination $destination -Filter {*.txt} -Force -Recurse
+
+Write-Output "Done...`n"
+
+$files = Get-ChildItem $destination
+
+$files | ForEach-Object {
+    Write-Output "Updating $destination\$_ for host: $env:computername`n"
+    (gc $destination\$_).replace('YOURMACHINENAME',"$env:computername")|sc "$destination\$_"
+}
+
